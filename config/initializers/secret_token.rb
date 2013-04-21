7ace641eced8ad23996175cb0b371d2db2c79889
Lloyd Taylor <ltaylor@netelder.com>
2013-04-21 10:21:40 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '4728676ec6a9cf99628f309ec31721106217da6560967dab2602a3adf3d6ea9bb77db0b42cbdfff969ce1b22605b5d547cf3f6f4b5c0932708424489164595a5'
